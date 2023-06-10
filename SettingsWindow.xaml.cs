0ad95480f2a3a0583b9c49c2cf628a8ad2734e68
Andrey Khachatryan <andrey@omg.lol>
2023-06-10 19:29:33 +0300

    Увеличил минимально принимаемый размер шрифта

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace Editor
         public static bool IsValid(string str)
         {
             int i;
-            return int.TryParse(str, out i) && i >= 1 && i <= 48;
+            return int.TryParse(str, out i) && i >= 6 && i <= 48;
         }
 
         public void FillFontComboBox(System.Windows.Controls.ComboBox comboBoxFonts)
